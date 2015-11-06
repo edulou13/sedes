a07281fa3d3adbe15b247f7717573b86b5a207de
Luis Eduardo Miranda Barja <mbarjaedu13@gmail.com>
2015-11-06 14:50:29 -0400

    modified:   sync_db.py

--- sync_db.py
+++ sync_db.py
@@ -61,7 +61,7 @@ def sync_db(localhost=True):
 	try:
 		sync_status()
 	except Exception, e:
-		print e
+		print 'Error at sync_db: {}'.format(e)
 
 def sync_status():
 	if utc.now().hour==14:
@@ -83,8 +83,8 @@ def sync_status():
 
 if __name__ == '__main__':
 	localhost = argv[1]=='--localhost' if len(argv)==2 else False
-	sc.every(10).seconds.do(sync_db, localhost)
-	# sc.every(1).minutes.do(sync_db, localhost)
+	# sc.every(10).seconds.do(sync_db, localhost)
+	sc.every(5).minutes.do(sync_db, localhost)
 	# sc.every().day.at('11:08').do(sync_db, localhost)
 	# sc.every().hours.do(sync_db, localhost)
 	# sc.every().day.at('07:50').do(sync_db, localhost)
